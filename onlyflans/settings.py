cdecb3ebf171956e440f652b59e89a7c8e819c31
NathalieMiño <nathalie.minosaez@gmail.com>
2024-05-04 00:17:58 -0400

    commit_hito2

--- onlyflans/settings.py
+++ onlyflans/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #mi app
+    'web'
 ]
 
 MIDDLEWARE = [
@@ -47,6 +49,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
+
 ]
 
 ROOT_URLCONF = 'onlyflans.urls'
@@ -121,3 +125,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+#################################################
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
--- onlyflans/urls.py
+++ onlyflans/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from web.views import indice, bienvenido, acerca
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', indice, name="indice"),
+    path('acerca/', acerca, name="acerca"),
+    path('bienvenido/', bienvenido, name="bienvenido"),
 ]
